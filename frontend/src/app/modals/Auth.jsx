4f3a31fbcad68a1ce6863dbdb0aeb5b7d6412207
Jamari Mitchell <92458788+yaboijams@users.noreply.github.com>
2025-03-26 17:29:09 -0500

    signin form animation

--- frontend/src/app/modals/Auth.jsx
+++ frontend/src/app/modals/Auth.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const Auth = ({ isOpen, onClose, children }) => {
   if (!isOpen) return null;
   return (
-    <div className="fixed inset-0 z-[1100] flex items-center justify-center bg-blue">
+    <div className="fixed inset-0 z-[1100] flex items-center justify-center animated-gradient">
       <div className="bg-white p-6 rounded shadow-lg relative max-w-md w-full">
         <button
           onClick={onClose}
